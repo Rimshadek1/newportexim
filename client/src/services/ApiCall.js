3bca0338eb2b89e732a4ef45d4c2b600da4f0b92
Rimshad <rmzrimshad1@gmail.com>
2024-02-22 15:12:43 +0530

    ready to host 38

--- client/src/services/ApiCall.js
+++ client/src/services/ApiCall.js
@@ -1,37 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const commonrequest = async (method, url, data) => {
-    try {
-        const response = await axios({
-            method,
-            url,
-            data
-            // You can add headers if needed
-        });
-        return response.data;
-    } catch (error) {
-        // Handle errors here
-        console.error('Request failed:', error);
-        throw error; // Rethrow the error to be caught by the caller
+export const commonrequest = async (methods, body, header) => {
+    let config = {
+        method: methods,
+        headers: header ? header
+            : {
+                "Content-Type": "application/json"
+            },
+        data: body
     }
-};
 
-// import axios from "axios";
-
-// export const commonrequest = async (methods, body, header) => {
-//     let config = {
-//         method: methods,
-//         headers: header ? header
-//             : {
-//                 "Content-Type": "application/json"
-//             },
-//         data: body
-//     }
-
-//     // axios instance
-//     return axios(config).then((data) => {
-//         return data
-//     }).catch((error) => {
-//         return error
-//     })
-// }
+    // axios instance
+    return axios(config).then((data) => {
+        return data
+    }).catch((error) => {
+        return error
+    })
+}
