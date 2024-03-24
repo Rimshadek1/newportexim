d7b9f27fe09f6e91fa524767f0dbef39890f6411
Rimshad <rmzrimshad1@gmail.com>
2024-03-24 11:05:50 +0530

    ready to host 60

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Components/css/style.css'
 import './Components/css/bootstrap.min.css'
+import { RoleProvider } from './pages/userContext/RoleContext';
+import { UserContextProvider } from './pages/userContext/Usercontext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RoleProvider>
+        <UserContextProvider>
+          <App />
+        </UserContextProvider>
+      </RoleProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
